e14af363b71494a2c86fd4ef6094513761afd5df
LuLuWen <38238159+LuLuWen@users.noreply.github.com>
2020-09-15 23:42:03 +0800

    Add files via upload

---
+++ src/test/java/com/demo/spring/mongoDB/MongoDbAtlasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.spring.mongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbAtlasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
